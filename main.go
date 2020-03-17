a032ee90948c48e361847bae11ff0abfcc5abaef
Rui Richard Blaese <ruiblaese@gmail.com>
2020-03-17 18:40:17 -0300

    fix port env heroku

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	router := gin.Default()
 
 	// Query string parameters are parsed using the existing underlying request object.
 	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-
 	router.GET("/welcome", welcome)
 	router.GET("/", welcome)
-	router.Run(":8080")
+	router.Run(port)
 }
 
 func welcome(c *gin.Context) {
